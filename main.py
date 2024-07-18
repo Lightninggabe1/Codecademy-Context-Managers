bd3a598017420b339f82c625b595adb3f32a5905
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-18 12:45:42 +0800

    Class Based Context Managers
    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ finally:
 
 with open('file_name.txt','r') as open_file:
   print(open_file.read())
+
+#Class Based Context Managers
+# Write your code below: 
+class PoemFiles:
+  def __init__(self):
+    print('Creating Poems!')
+  def __enter__(self):
+    print('Opening poem file')
+  def __exit__(self,*exc):
+    print('Closing poem file')
+
+with PoemFiles() as manager:
+  print('Hope is the thing with feathers')
