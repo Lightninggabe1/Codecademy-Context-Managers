1181f0a550288909a403b1750b241f66bc985828
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-19 16:48:53 +0800

    Handling Exceptions I
    Update main.py
    
    5/10
    
    11 min

--- main.py
+++ main.py
@@ -40,3 +40,32 @@ class PoemFiles:
 
 with PoemFiles('poem.txt', 'w') as open_poem_file:
   open_poem_file.write('Hope is the thing with feathers')
+
+#Handling Exceptions I
+class PoemFiles:
+
+  def __init__(self, poem_file, mode):
+    print(' \n -- Starting up a poem context manager -- \n ')
+    self.file = poem_file
+    self.mode = mode
+
+  def __enter__(self):
+    print('Opening poem file')
+    self.opened_poem_file = open(self.file, self.mode)
+    return self.opened_poem_file
+
+  # Create your __exit__ method here:
+  def __exit__(self,exc_type,exc_value,traceback):
+    print(exc_type)
+    print(exc_value)
+    print(traceback)
+    self.opened_poem_file.close()
+# First
+# with PoemFiles('poem.txt', 'r') as file:
+  # print("---- Exception data below ----")
+  # print(file.uppercasewords())
+
+# Second
+with PoemFiles('poem.txt', 'r') as file2:
+  print(file2.read()) 
+  print("---- Exception data below ----")
