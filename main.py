e9ef4c0b36cb425f4dbf50649812b5a6649ee8c1
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-22 10:36:06 +0800

    Introduction to Contextlib
    I
    n
    t
    r
    o
    d
    u
    c
    t
    i
    o
    n
    
    t
    o
    
    C
    o
    n
    t
    e
    x
    t
    l
    i
    b
    
    7/10 of Context Managers

--- main.py
+++ main.py
@@ -99,16 +99,18 @@ with PoemFiles('poem.txt', 'r') as file2:
   print(" \n ---- Exception data below ---- \n ")
 #Introduction to Contextlib
 # Write your code below:
+from contextlib import contextmanager
 
-
-
-
-
-
-
-
-
-# with poem_files('poem.txt', 'a') as opened_file:
-#  print('Inside yield')
-#  opened_file.write('Rose is beautiful, Just like you.')
+@contextmanager
+def poem_files(file,mode):
+  print('Opening File')
+  open_poem_file = open(file,mode)
+  try:
+    yield open_poem_file
+  finally:
+    print("Closing File")
+    open_poem_file.close()
+with poem_files('poem.txt', 'a') as opened_file:
+  print('Inside yield')
+  opened_file.write('Rose is beautiful, Just like you.')
 
