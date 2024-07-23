c138e658d181979740a07050ece9bbb5ffaa7c9d
Lightninggabe1 <lightninggabe191@gmail.com>
2024-07-23 15:03:09 +0800

    Contextlib Error Handling
    C
    o
    n
    t
    e
    x
    t
    l
    i
    b
    
    E
    r
    r
    o
    r
    
    H
    a
    n
    d
    l
    i
    n
    g

--- main.py
+++ main.py
@@ -114,3 +114,25 @@ with poem_files('poem.txt', 'a') as opened_file:
   print('Inside yield')
   opened_file.write('Rose is beautiful, Just like you.')
 
+
+
+#Contextlib Error Handling
+from contextlib import contextmanager
+ 
+@contextmanager
+def poem_files(file, mode):
+  print('Opening File')
+  open_poem_file = open(file, mode)
+  try:
+    yield open_poem_file
+  #Write your code below: 
+  except AttributeError as e:
+    print(e)
+  finally:
+    print('Closing File')
+    open_poem_file.close()
+
+with poem_files('poem.txt', 'a') as opened_file:
+  print('Inside yield')
+  opened_file.sign('Buzz is big city. big city is buzz.')
+
